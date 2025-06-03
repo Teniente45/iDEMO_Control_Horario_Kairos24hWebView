faef0c3e5f3159411693a27d43be4a911f91bc11
juanlopezmarin <Juanteniente45@gmail.com>
2025-06-04 00:23:31 +0200

    111

--- app/src/main/java/com/miapp/kairos24h/paginaLogin.kt
+++ app/src/main/java/com/miapp/kairos24h/paginaLogin.kt
@@ -149,7 +149,7 @@ class MainActivity : ComponentActivity() {
                                                             xEmpleado.tLogo,
                                                             xEmpleado.lTipo
                                                         )
-                                                        val lTipo = "TABLET" // Valor fijo para forzar modo TABLET
+                                                        val lTipo = "TABLET"
                                                         android.util.Log.d("Redireccion", "Valor de lTipo: $lTipo")
                                                         if (lTipo == "TABLET") {
                                                             android.util.Log.d("Redireccion", "Iniciando MainActivity (modo TABLET)")
